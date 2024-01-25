1c9b98d6afee84a26a08cdae2687d63dc1bf6648
KushJuneja <157740417+KushJuneja@users.noreply.github.com>
2024-01-25 10:48:10 -0800

    Add files via upload
    RUN EXE
    **
    Calculator\obj\Release\Calculator.exe"
    **
    
    Building a Windows Form Calculator App in C#
    
    Description: This project involves using Visual Studio to construct a Windows Form desktop application, focusing on a calculator program. Key learning objectives include:
    
    1. Visual Component Construction: Mastering the use of Visual Studio tools to build the GUI (Graphical User Interface) of a C# Windows Form application. This includes declaring, creating, configuring, positioning, and rendering various visual controls and components necessary for the interface.
    
    2. Event Handling: Learning the intricacies of the delegation event handling framework within C#. Developing the logic behind the calculator's response to user interactions, closely referencing the standard Windows desktop calculator for functional guidance and mimicking its user input response mechanisms.

---
+++ Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿//////////////////////////////////////////////////////////////////////////
+/// Kush Juneja
+/// TINFO-200 CW Programming
+/// Lcalc - the GUI calculator assignment
+/// 
+/////////////////////////////////////////////////////////////////////////
+/// Change History
+/// Date        Developer           Description
+/// 2024-01-15  Kush Juneja         Initial creation of program, app, and this file
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    // The starting point for the application / GUI components
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
